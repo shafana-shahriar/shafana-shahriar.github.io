d06661ddcb95dca8d15653e86c6a88cc337cdca6
shafana-shahriar <shafana.shahriar@gmail.com>
2021-11-14 21:58:21 +0400

    update

---
+++ variables and datatypes/srcipt.js
@@ -0,0 +1,28 @@
+/* VARIABLE AND DATA TYPES */
+
+// STRING
+let myFirstJob = 'Developer';
+console.log(myFirstJob);
+
+let myCurrentJob = 'Mentor';
+console.log(myCurrentJob);
+
+// NUMBER
+let age = 28;
+console.log(age);
+
+// BOOLEAN
+let programmingIsFun = true;
+console.log(programmingIsFun);
+
+// REASSIGN A VALUE
+programmingIsFun = 'Yes!'
+console.log(programmingIsFun);
+
+// UNDEFINED
+let year;
+console.log(year);
+
+// NULL
+let x = null
+console.log(x);
